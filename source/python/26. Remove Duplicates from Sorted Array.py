28301ceeebabf2715413ed354e5da5b894cb9ef6
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2023-01-21 23:19:30 +0600

    💡 Solved: 26. Remove Duplicates from Sorted Array

---
+++ source/python/26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+
+
+def test():
+    s = Solution()
+
+    nums = [1, 1, 2]
+    assert s.removeDuplicates(nums) == 2
+
+    nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    assert s.removeDuplicates(nums) == 5
+
+    print("All tests passed!")
+
+
+if __name__ == "__main__":
+    test()
